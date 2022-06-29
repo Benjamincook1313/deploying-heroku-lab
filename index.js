cfff556dec46a12df804aa973c413eef5147aa2a
Benjamin Cook <benjamin.cook@devmounta.in>
2022-06-29 16:56:47 -0600

    add video

---
+++ index.js
@@ -0,0 +1,8 @@
+
+const clickMe = document.querySelector('#click-me')
+
+function tailGrab(){
+    alert('Tail Grab style points = 500')
+};
+
+clickMe.addEventListener('click', tailGrab)
--- server/server.js
+++ server/server.js
@@ -9,4 +9,9 @@ app.get('/', (req, res) =>{
     res.sendFile(path.join(__dirname, '../index.html'))
 })
 
+app.get('/css', (req, res) => {
+    res.sendFile(path.join(__dirname, '../styles.css'))
+})
+
+
 app.listen(port, () => console.log(`Server running on port ${port}`))
